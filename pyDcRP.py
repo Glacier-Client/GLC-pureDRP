387c5097ccf925a86a307a9b30891075b70623d3
SpyMIner <vince051105@icloud.com>
2022-05-11 11:42:56 +0200

    Add files via upload

---
+++ pyDcRP.py
@@ -0,0 +1,22 @@
+from pypresence import Presence
+import PySimpleGUI as sg
+import time
+
+layout = [[sg.Text("GLC-DRP is active!")], [sg.Button("close")]]
+window = sg.Window("GLC-DRP", layout)
+
+client_id = "944777934915399691"  # Enter your Application ID here.
+RPC = Presence(client_id=client_id)
+RPC.connect()
+
+
+# Make sure you are using the same name that you used when uploading the image
+start_time=time.time() # Using the time that we imported at the start. start_time equals time.
+RPC.update(large_image="large", state="Playing Minecraft 1.8.8", start=start_time) # We want to apply start time when you run the presence.
+
+while 1:
+    event, values = window.read()
+    if event == "close" or event == sg.WIN_CLOSED:
+        break
+RPC.clear()
+window.close()
